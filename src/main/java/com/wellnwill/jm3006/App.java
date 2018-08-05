0603d6ff8829493d53989626a765824aac9dcb20
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-08-05 11:36:16 +0530

    initial commit

---
+++ src/main/java/com/wellnwill/jm3006/App.java
@@ -0,0 +1,13 @@
+package com.wellnwill.jm3006;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
